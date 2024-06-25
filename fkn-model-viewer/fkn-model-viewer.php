cc691b08b6125f1e53c201ba9f126a7e97b3be05
Jesus Rumie Vittar <23528273+jrumievittar@users.noreply.github.com>
2024-06-25 20:07:08 +0200

    Add files via upload

---
+++ fkn-model-viewer/fkn-model-viewer.php
@@ -0,0 +1,197 @@
+<?php
+/**
+ * Plugin Name: FKN Model Viewer
+ * Plugin URI: https://wordpress.org/plugins/fkn-model-viewer
+ * Description: This is a plugin to insert the model-viewer script and generate shortcode to insert a 3d model in the frontend web page.
+ * Version: 1.0.0
+ * Author: J. Rumie Vittar by fuken
+ * Author URI: https://fuken.es/
+ * Author Email: contacto@fuken.es
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+
+// Add script -------------------------------
+function fkn_mv_mas_add_script() {
+    wp_enqueue_script( 'model-viewer', plugin_dir_url( __FILE__ ) . 'public/model-viewer-lib.js', array(), null, false );
+    wp_enqueue_script_module('model-viewer', plugin_dir_url( __FILE__ ) . 'public/model-viewer-lib.js');
+}
+
+add_action('wp_enqueue_scripts', 'fkn_mv_mas_add_script');
+// ---------------------------------------------------
+
+
+
+
+// Add the menu ------------------------------
+ function fkn_mv_am_add_admin_menu() {
+    // Add the new submenu to the main administration menu
+    add_menu_page( 
+
+        'fkn Model Viewer', // Submenu title
+        'fkn Model Viewer', // Menu title
+        'manage_options', // Capability
+        'fkn_model_viewer_settings', // Settings page function
+        'fkn_mv_p_page' // Function to display the settings page
+    );
+}
+add_action('admin_menu', 'fkn_mv_am_add_admin_menu'); // Hook into the admin_menu action
+// ---------------------------------------------------
+
+
+
+// Admin page function ------------------------
+function fkn_mv_p_page() {
+
+    // Option controls
+    if (!current_user_can('manage_options')) {
+        wp_die('You do not have sufficient permissions to access this page.');
+    }
+
+    // Handle form submission
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['model_viewer_save'])) {
+
+        // Verificar nonce
+        if ( ! isset( $_POST['model_viewer_nonce'] ) || ! wp_verify_nonce( sanitize_text_field( wp_unslash ( $_POST['model_viewer_nonce'] ) ) , 'model_viewer_save' ) ) {
+            wp_die( 'Nonce verification failed.' );
+        }
+        
+        // Save the shortcode
+        $file_id = fkn_mv_handle_file_upload();
+        $shortcode = fkn_mv_gs_generate_shortcode($file_id);
+        echo '<div class="updated"><p>Saved shortcode: ' . esc_html($shortcode) . '</p></div>';
+    }
+
+    // Code for the admin page functionality form
+    echo '<div class="wrap">';
+    echo '<h2>fkn Model Viewer Options</h2>';
+    echo '<p>Please select a .glb file:</p>';
+    echo '<br>';
+    // Mostrar el nonce
+    echo '<form method="post" enctype="multipart/form-data">';
+    wp_nonce_field( 'model_viewer_save', 'model_viewer_nonce' );
+    echo '<input type="file" name="model_viewer_file" required>';
+    echo '<br><br>';
+    echo '<input type="submit" name="model_viewer_save" class="button button-primary" value="Save Shortcode">';
+    echo '</form>';
+
+    // Instructions for use
+    echo '<br><br><br>';
+    echo '<h3>Instructions for use:</h3>';
+    echo '<p>To use the plugin, simply select a .glb file in the file field and press the “Save Shortcode” button.</p>';
+    echo '<p>The shortcode can be copied and pasted anywhere on the website.</p>';
+    echo '<h4>How to Fix: “Sorry, This File Type is Not Allowed for Security Reasons” in WordPress</h4>';
+    echo '<ul>';
+    echo '<li><strong>Using a WordPress plugin</strong></li>';
+    echo '<ol>';
+    echo '<li>Downloading the free plugin <a href="https://es.wordpress.org/plugins/wp-extra-file-types/" target="_blank">Extra File Types</a></li>';
+    echo '</ol>';
+    echo '</ul>';
+
+    // Regard 
+    echo '<br>';
+    echo '<p>Made with ❤️. Thank you for using <strong>fkn Model Viewer</strong> by <strong>fuken</strong>. Please consider making a donation to support <a href="https://paypal.me/jrumievittar?country.x=ES&locale.x=es_ES" target="_blank">this project</a>.</p>';
+    echo '</div>';
+}
+// ---------------------------------------------------
+
+
+
+
+// Handle file upload --------------------------
+function fkn_mv_handle_file_upload() {
+    $file = sanitize_file_name($_FILES['model_viewer_file']); //$file = $_FILES['model_viewer_file'];
+
+    // Verificar si hay errores en la carga del archivo
+    if ($file['error'] !== UPLOAD_ERR_OK) {
+        wp_die('Error loading file.');
+    }
+
+
+    // Verificar nonce
+    if (!isset($_POST['model_viewer_nonce']) || !wp_verify_nonce(sanitize_text_field(wp_unslash($_POST['model_viewer_nonce'])), 'model_viewer_save')) {
+        wp_die('Nonce verification failed.');
+    }
+
+    // Asegurarse de que el archivo fue subido
+    if (!isset($_FILES['model_viewer_file']) || !is_array($_FILES['model_viewer_file'])) {
+        wp_die('No file uploaded.');
+    }
+
+
+    // Saneamiento de datos del archivo
+    $file = array_map('sanitize_text_field', $file);
+
+
+    // Usar wp_handle_upload para manejar la carga del archivo
+    $upload_overrides = array( 'test_form' => false );
+    $upload_info = wp_handle_upload($file, $upload_overrides);
+
+    if (isset($upload_info['error'])) {
+        wp_die($upload_info['error']);
+    }
+
+    // Sanear el nombre del archivo
+    $file_name = sanitize_file_name($upload_info['file']);
+
+    // Validar que se haya seleccionado un archivo
+    if (empty($file_name)) {
+        wp_die('Error: No file selected.');
+    }
+
+    // Crear la entrada del archivo adjunto en la base de datos de WordPress
+    $attachment = array(
+        'post_title'     => $file_name,
+        'post_content'   => '',
+        'post_status'    => 'inherit',
+        'post_mime_type' => $upload_info['type'],
+    );
+
+    $file_id = wp_insert_attachment($attachment, $upload_info['file']);
+
+    return $file_id;
+}
+// ---------------------------------------------------
+
+
+
+
+
+// Generate shortcode -----------------------------
+function fkn_mv_gs_generate_shortcode($file_id) {
+    $file_url = wp_get_attachment_url($file_id);
+    $shortcode = '[fkn_mv_s_shortcode src="' . esc_url($file_url) . '"]';
+    return $shortcode;
+}
+// ---------------------------------------------------
+
+
+
+
+// Shortcode function to display the shortcode in the editor -----------------
+function fkn_mv_s_shortcode($atts) {
+    $atts = shortcode_atts(
+        array(
+            'src' => '',
+        ),
+        $atts,
+        'fkn_mv_s_shortcode'
+    );
+
+    if (empty($atts['src'])) {
+        return 'Error: Atributo "src" requerido.';
+    }
+
+    $output = '<model-viewer loading="eager" camera-controls touch-action="pan-y" auto-rotate src="' . esc_url($atts['src']) . '" shadow-intensity="1" alt="A 3D model" style="width: 350px; height: 300px;"></model-viewer>';
+
+    return $output;
+}
+add_shortcode('fkn_mv_s_shortcode', 'fkn_mv_s_shortcode');
+// ---------------------------------------------------
---
+++ fkn-model-viewer/readme.txt
@@ -0,0 +1,62 @@
+=== FKN Model Viewer ===
+Contributors: J. Rumie Vittar by fuken
+Tags: model-viewer , 3d, model-viewer-3d, Aumented Reality
+Requires at least: 5.0.0
+Tested up to: 6.5.4
+Requires PHP: 7.0
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.en.html
+Donate link: https://paypal.me/jrumievittar?country.x=ES&locale.x=es_ES
+
+ The inclusion of 3D products on a website is a trend that not only impresses visually, but transforms the online shopping experience.
+
+
+== Description ==
+fuken Model Viewer is a plugin that allows you to insert and interact with 3d objects on your WordPress website. The adoption of this innovative technology in the presentation of products online is a strategic step for any business that seeks to stand out, offer differentiated value and increase customer satisfaction. These types of objects on the web will increase user engagement and increase your site's revenue.
+
+
+== Features ==
+1. No flash needed
+2. Compatible with modern browsers such as Chrome, Firefox, Edge and Safari.
+3. Augmented reality support
+4. Supports both mouse and touch.
+5. You can zoom in on the 3d object using touch.
+
+
+== Instalation ==
+1. Download the plugin, unzip it and move the unzipped folder to the "wp-content/plugins" directory in your WordPress installation.
+2. In your admin panel, go to Plugins and you'll find FKN Model Viewer in the plugins section.
+3. Enable the option to upload .glb files in your wordpress admin
+4. Click on the 'Activate' button to use your new plugin right away.
+5. Done
+
+
+==How to use==
+1. After installing and activating the fuken Model Viewer plugin on your WordPress website.
+2. Upload a .glb file and click save
+3. Copy the shortcode
+4. Navigate to your blog post or page and use the editor to add a shortcode.
+5. Click "Publish" to see your new web page with an interactive 3D object.
+6. Congratulations, you are already enjoying your plugin
+
+
+== Screenshots ==
+1. Front end model viewer 3D
+
+== Frequently Asked Questions ==
+1. No technical skills needed.
+
+== Changelog ==
+= 1.0.0 - Feb 25 2024 =
+* Initial release
+
+== Upgrade Notice == 
+This is first version no known notices yet
+
+
+fkn Model Viewer WordPress Plugin, Copyright 2024 J. Rumie Vittar.
+fkn Model Viewer is distributed under the terms of the GNU GPL.
+
+
+
